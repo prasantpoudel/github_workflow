a7f6cdecce071b155155be7dcaac6942c034c8c5
Prasant Poudel <poudelprasant88@gmail.com>
2023-08-23 17:25:33 +0545

    first init

---
+++ app/main_test.py
@@ -0,0 +1,15 @@
+from main import return_backwards_string, get_mode 
+import unittest 
+import os
+
+class TestMain(unittest.TestCase):
+    def test_return_backward_string(self):
+        random_string = 'this is test string'
+        random_string_reversed = 'gnirts tset si siht'
+        self.assertEqual(random_string_reversed,return_backwards_string(random_string))
+
+    def test_get_env(self):
+        self.assertEqual(os.environ.get('MODE'), get_mode())
+
+if __name__=="__main__":
+    unittest.main()
